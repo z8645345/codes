14c33aed04ee160cffd1a2c04d074969294b8035
z8645345 <1294607595@qq.com>
2020-12-31 13:43:03 +0800

    二叉搜索树add

--- src/main/java/二叉搜索树/SearchBinaryTree.java
+++ src/main/java/二叉搜索树/SearchBinaryTree.java
@@ -1,72 +1,60 @@
 package 二叉搜索树;
 
-import java.util.LinkedList;
+public class SearchBinaryTree<E> {
 
-public class SearchBinaryTree {
-
-    private Node root;
+    private Node<E> root;
+    private int size;
 
     /**
      * 添加节点
-     * @param item
+     * @param element
      */
-    public Integer add(Integer item) {
-        if (root == null) {
-            root = new Node(item);
-            return null;
+    public void add(E element) {
+        elementNotNullCheck(element);
+    }
+
+    private void elementNotNullCheck(E e) {
+        if (e == null) {
+            throw new IllegalArgumentException("element not be null ……");
         }
-        Node parent = root;
-        while (true) {
-            if (item > parent.item) {
-                // 在左节点
-                if (parent.left == null) {
-                    parent.left = new Node(item);
-                    return null;
-                }
-                parent = parent.left;
-            } else if(item < parent.item) {
-                // 在由节点
-                if (parent.right == null) {
-                    parent.right = new Node(item);
-                    return null;
-                }
-                parent = parent.right;
+        if (root == null) {
+            root = new Node<>(e, null);
+            return;
+        }
+        Node<E> node = root;
+        Node<E> parent = null;
+        int com = 0;
+        while (node != null) {
+            com = compar(e, node.element);
+            parent = node;
+            if (com > 0) {
+                node = node.right;
+            } else if (com < 0) {
+                node = node.left;
             } else {
-                // 相等
-                Integer old = parent.item;
-                parent.item = item;
-                return item;
+                return;
             }
         }
+        if (com > 0) {
+            parent.right = new Node<>(e, parent);
+        } else if (com < 0) {
+            parent.left = new Node<>(e, parent);
+        }
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        Node parent = root;
-        while (parent != null) {
-            sb.append(parent.item);
-            if (parent.)
-        }
-        return "SearchBinaryTree{" +
-                "root=" + root +
-                '}';
+    private int compar(E e1, E e2) {
+        return 0;
     }
 
-    private static class Node {
-        Integer item;
-        Node parent;
-        Node left;
-        Node right;
+    private static class Node<E> {
+        E element;
+        Node<E> parent;
+        Node<E> left;
+        Node<E> right;
 
-        public Node(Integer item) {
-            this(item, null, null);
-        }
-
-        public Node(Integer item, Node left, Node right) {
-            this.item = item;
-            this.left = left;
-            this.right = right;
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
         }
     }
 }
